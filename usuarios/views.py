da6ddceaca82bbafbe11f433efffceb4bb15bef1
benjamin_bravo88294 <165076529+Prepucioman777@users.noreply.github.com>
2025-11-14 19:57:23 -0300

    Comment out qrcode import in views.py
    comente la linea 11 de la vista de usuarios para evitar error al hacer migrate

--- usuarios/views.py
+++ usuarios/views.py
@@ -8,7 +8,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from .forms import RegistroForm, LoginForm  # , ReviewForm  
 from .models import User  # , Review  
-import qrcode
+# import qrcode COMENTADO PARA EVITAR ERROR AL HACER MIGRACIÓN
 from io import BytesIO
 import base64
 
